2560758c66a49657ac48d6d07bf2e1a8e30cb4fa
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-03 19:04:15 +0530

    Update main.tf

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -31,3 +31,23 @@ resource "kubernetes_deployment" "appointment_service" {
     }
   }
 }
+resource "kubernetes_service" "appointment_service" {
+  depends_on = [kubernetes_deployment.appointment_service]  # Ensure deployment is ready
+
+  metadata {
+    name = "appointment-service"
+  }
+
+  spec {
+    selector = {
+      app = "appointment-service"
+    }
+
+    port {
+      port        = 80
+      target_port = 3001
+    }
+
+    type = "LoadBalancer"
+  }
+}
