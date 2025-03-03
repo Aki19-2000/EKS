3c7b201ad0b78107f0ced35a8f30b4f42e6aa46f
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-03 19:07:03 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+# terraform/variables.tf (root-level variables)
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the EKS nodes"
+  type        = list(string)
+}
+
+variable "instance_types" {
+  description = "List of EC2 instance types for the worker nodes"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the EKS cluster and security group will be deployed"
+  type        = string
+}
