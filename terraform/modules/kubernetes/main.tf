e39e66732555b7535d7ca833fca34f8ebe9ccdc2
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-03 19:03:46 +0530

    Update main.tf

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -1,4 +1,6 @@
 resource "kubernetes_deployment" "appointment_service" {
+  depends_on = [aws_eks_node_group.node_group]  # Ensure node group is ready
+
   metadata {
     name      = "appointment-service"
     namespace = "default"
@@ -6,48 +8,26 @@ resource "kubernetes_deployment" "appointment_service" {
 
   spec {
     replicas = 2
-
     selector {
       match_labels = {
         app = "appointment-service"
       }
     }
-
     template {
       metadata {
         labels = {
           app = "appointment-service"
         }
       }
-
       spec {
         container {
           name  = "appointment-service"
-          image = "510278866235.dkr.ecr.us-east-1.amazonaws.com/appointment-service:latest"
+          image = "your_ecr_image_url"
           port {
             container_port = 3001
           }
         }
       }
     }
-  }
-}
-
-resource "kubernetes_service" "appointment_service" {
-  metadata {
-    name = "appointment-service"
-  }
-
-  spec {
-    selector = {
-      app = "appointment-service"
-    }
-
-    port {
-      port        = 80
-      target_port = 3001
-    }
-
-    type = "LoadBalancer"
   }
 }
