75db8c477d37c01239e377e485a36413901b2a9e
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-03 19:03:26 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,4 +1,3 @@
-# Create an IAM role for EKS cluster
 resource "aws_iam_role" "eks_role" {
   name               = "eks-cluster-role"
   assume_role_policy = jsonencode({
@@ -16,13 +15,11 @@ resource "aws_iam_role" "eks_role" {
   })
 }
 
-# Attach the Amazon EKS Cluster Policy to the IAM role
 resource "aws_iam_role_policy_attachment" "eks_policy" {
   role       = aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# Create an IAM role for EKS node group
 resource "aws_iam_role" "eks_node_role" {
   name               = "eks-node-group-role"
   assume_role_policy = jsonencode({
@@ -40,16 +37,20 @@ resource "aws_iam_role" "eks_node_role" {
   })
 }
 
-# Attach the Amazon EKS Worker Node Policy to the IAM role
+# Attach policies to the IAM role for the EKS node group
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-# Attach the Amazon EC2 Container Registry Read-Only Policy to the IAM role
 resource "aws_iam_role_policy_attachment" "eks_worker_ecr_policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_eks_cluster" "main" {
@@ -57,13 +58,12 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.eks_role.arn  # Reference the IAM role for EKS cluster
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids             = var.subnet_ids
     endpoint_public_access = true
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_policy]
 }
-
 
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.main.name
@@ -71,6 +71,7 @@ resource "aws_eks_node_group" "node_group" {
   node_role_arn   = aws_iam_role.eks_node_role.arn  # Use the IAM role for the node group
   subnet_ids      = var.subnet_ids
   instance_types  = var.instance_types
+
   scaling_config {
     desired_size = 2
     max_size     = 3
