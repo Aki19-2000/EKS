896434208cc8b7ee231641a6b07ef7f09d697a48
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 18:43:39 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,5 @@
-# terraform/variables.tf (root-level variables)
-
-variable "cluster_name" {
-  description = "The name of the EKS cluster"
+variable "region" {
+  description = "The AWS region to deploy to"
   type        = string
-}
-
-variable "subnet_ids" {
-  description = "List of subnet IDs for the EKS nodes"
-  type        = list(string)
-}
-
-
-variable "instance_types" {
-  description = "List of EC2 instance types for the worker nodes"
-  type        = list(string)
-  default     = ["t3.medium"]  # You can change this as needed
-}
-
-
-variable "vpc_id" {
-  description = "VPC ID where the EKS cluster and security group will be deployed"
-  type        = string
+  default     = "us-east-1"
 }
