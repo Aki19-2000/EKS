eeb74d543a074ff18c7ec88abf2483f7479e01e8
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 19:14:59 +0530

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,14 +1,10 @@
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.3.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = ">= 4.33.0"
     }
   }
 }
