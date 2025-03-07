1bf32e5660af5a931139006b7124803fca144f66
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 20:20:34 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -25,3 +25,14 @@ variable "subnet_ids" {
   description = "A list of subnets for the EKS cluster"
   type        = list(string)
 }
+output "cluster_id" {
+  value = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
