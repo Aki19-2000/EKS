79be746289ed6a3a25349d41de8f2ba3a1bd58bb
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 19:47:22 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -2,10 +2,20 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
-  subnets         = var.subnets
   vpc_id          = var.vpc_id
+  subnet_ids      = var.subnet_ids
 
-  node_groups = var.node_groups
+  node_groups = {
+    node_group_1 = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t3.medium"
+
+      key_name = "my-key"
+    }
+  }
 }
 
 variable "cluster_name" {
@@ -18,14 +28,14 @@ variable "cluster_version" {
   type        = string
 }
 
-variable "subnets" {
-  description = "A list of subnets for the EKS cluster"
-  type        = list(string)
-}
-
 variable "vpc_id" {
   description = "The VPC ID for the EKS cluster"
   type        = string
+}
+
+variable "subnet_ids" {
+  description = "A list of subnets for the EKS cluster"
+  type        = list(string)
 }
 
 variable "node_groups" {
