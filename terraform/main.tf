f37debee48193c842638c395d4c9bb71e4816a8f
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 17:34:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,15 +14,14 @@ module "vpc" {
 module "eks" {
   source        = "./modules/eks"
   cluster_name  = var.cluster_name
-  subnet_ids    = [module.vpc.subnet_public_id, module.vpc.subnet_private_id]  # Reference output values from VPC
+  subnet_ids    = [module.vpc.subnet_public_id, module.vpc.subnet_private_id]  # Use the outputs from the VPC module
   instance_types = var.instance_types
   vpc_id        = module.vpc.vpc_id  # Get VPC ID from module output
 }
 
-
 module "security_groups" {
   source = "./modules/security_groups"
-  vpc_id = module.vpc.vpc_id
+  vpc_id = module.vpc.vpc_id  # Use the VPC ID from the VPC module
 }
 
 module "kubernetes" {
