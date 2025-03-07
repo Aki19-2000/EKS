7d024a4fb1d2324a0361e703d0a7d280d36e847d
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 18:57:36 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "eks_cluster_id" {
   value = module.eks.cluster_id
 }
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
