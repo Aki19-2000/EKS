90be8ccc4a4a59200ae3959e11e9899a8b075799
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 19:48:01 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,8 @@ module "eks" {
   source          = "./modules/eks"
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.21"
-  subnets         = module.vpc.public_subnets
   vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.public_subnets
 
   node_groups = {
     node_group_1 = {
