1d409c930ce7bbb5ab00454bd59339d1d340c057
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 18:42:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,28 @@
 module "vpc" {
-  source               = "./modules/vpc"
-  cidr_block           = "10.0.0.0/16"
-  public_subnet_cidr   = "10.0.1.0/24"
-  private_subnet_cidr  = "10.0.2.0/24"
-  availability_zone_1  = "us-east-1a"
-  availability_zone_2  = "us-east-1b"
+  source            = "./modules/vpc"
+  cidr_block        = "10.0.0.0/16"
+  name              = "my-vpc"
+  public_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets   = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
 }
 
 module "eks" {
-  source        = "./modules/eks"
-  cluster_name  = var.cluster_name
-  subnet_ids    = [module.vpc.subnet_public_id, module.vpc.subnet_private_id]
-  instance_types = var.instance_types
-  vpc_id        = module.vpc.vpc_id
+  source          = "./modules/eks"
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.21"
+  subnets         = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
 
-  depends_on = [module.vpc]  # Add dependency on the VPC module
-}
+  node_groups = {
+    node_group_1 = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
 
-module "security_groups" {
-  source = "./modules/security_groups"
-  vpc_id = module.vpc.vpc_id
+      instance_type = "t3.medium"
 
-  depends_on = [module.vpc]  # Add dependency on the VPC module
-}
-
-module "kubernetes" {
-  source = "./modules/kubernetes"
-
-  depends_on = [module.eks]  # Add dependency on the EKS module
+      key_name = "my-key"
+    }
+  }
 }
