c7c98a4c066a8d0a4dcf6156b70e2fa3ccc64b71
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 20:07:00 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -4,8 +4,25 @@ module "eks" {
   cluster_version = var.cluster_version
   vpc_id          = var.vpc_id
   subnet_ids      = var.subnet_ids
+}
 
-  node_groups = var.node_groups
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = module.eks.cluster_id
+  node_group_name = "example"
+  node_role_arn   = aws_iam_role.node_group.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  remote_access {
+    ec2_ssh_key = "my-key"
+  }
 }
 
 variable "cluster_name" {
@@ -26,9 +43,4 @@ variable "vpc_id" {
 variable "subnet_ids" {
   description = "A list of subnets for the EKS cluster"
   type        = list(string)
-}
-
-variable "node_groups" {
-  description = "A map of node group configurations"
-  type        = map(any)
 }
