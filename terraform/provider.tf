40032dfa0d30ef45edb0675b9edcfe5b93d3ab29
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 22:02:48 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,9 +9,9 @@ provider "kubernetes" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
