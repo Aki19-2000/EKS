278a1d82fd08757d63f80399cd5f33b857173a11
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 18:42:05 +0530

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,116 +1,83 @@
-# Create a custom VPC
-resource "aws_vpc" "custom_vpc" {
+resource "aws_vpc" "this" {
   cidr_block = var.cidr_block
-  enable_dns_support = true
-  enable_dns_hostnames = true
 
   tags = {
-    Name = "Custom VPC"
+    Name = var.name
   }
 }
 
-# Create Public Subnet
-resource "aws_subnet" "subnet_public" {
-  vpc_id            = aws_vpc.custom_vpc.id
-  cidr_block        = var.public_subnet_cidr
-  availability_zone = var.availability_zone_1
-  map_public_ip_on_launch = true
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = element(var.public_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
 
   tags = {
-    Name = "Public Subnet"
+    Name = "${var.name}-public-${count.index}"
   }
 }
 
-# Create Private Subnet
-resource "aws_subnet" "subnet_private" {
-  vpc_id            = aws_vpc.custom_vpc.id
-  cidr_block        = var.private_subnet_cidr
-  availability_zone = var.availability_zone_2
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = element(var.private_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
 
   tags = {
-    Name = "Private Subnet"
+    Name = "${var.name}-private-${count.index}"
   }
 }
 
-# Create Internet Gateway and attach it to the VPC
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.custom_vpc.id
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
 
   tags = {
-    Name = "Internet Gateway"
+    Name = var.name
   }
 }
 
-# Create Elastic IP for NAT Gateway
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
-# Create NAT Gateway in the public subnet
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.subnet_public.id
-
-  depends_on = [aws_internet_gateway.main]
-
-  tags = {
-    Name = "NAT Gateway"
-  }
-}
-
-# Create Route Table for Public Subnet
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.custom_vpc.id
+  vpc_id = aws_vpc.this.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.this.id
   }
 
   tags = {
-    Name = "Public Route Table"
+    Name = "${var.name}-public"
   }
 }
 
-# Associate Public Subnet with Route Table
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.subnet_public.id
+  count          = length(var.public_subnets)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public.id
 }
 
-# Create Route Table for Private Subnet
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.custom_vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id  # Route internet traffic through NAT Gateway
-  }
-
-  tags = {
-    Name = "Private Route Table"
-  }
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
 }
 
-# Associate Private Subnet with Route Table
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.subnet_private.id
-  route_table_id = aws_route_table.private.id
+variable "name" {
+  description = "The name of the VPC"
+  type        = string
 }
 
-# Output the Public Subnet ID
-output "subnet_public_id" {
-  value = aws_subnet.subnet_public.id
+variable "public_subnets" {
+  description = "A list of public subnet CIDR blocks"
+  type        = list(string)
 }
 
-# Output the Private Subnet ID
-output "subnet_private_id" {
-  value = aws_subnet.subnet_private.id
+variable "private_subnets" {
+  description = "A list of private subnet CIDR blocks"
+  type        = list(string)
 }
 
-# Output the VPC ID
-output "vpc_id" {
-  value = aws_vpc.custom_vpc.id
+variable "availability_zones" {
+  description = "A list of availability zones"
+  type        = list(string)
 }
-
-
