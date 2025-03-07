7bda51585bb589b930dda7d3f423ce3f17f7a908
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-07 20:12:36 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -6,25 +6,6 @@ module "eks" {
   subnet_ids      = var.subnet_ids
 }
 
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = module.eks.cluster_id
-  node_group_name = "example"
-  node_role_arn   = aws_iam_role.node_group.arn
-  subnet_ids      = var.subnet_ids
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
-
-  instance_types = ["t3.medium"]
-
-  remote_access {
-    ec2_ssh_key = "my-key"
-  }
-}
-
 variable "cluster_name" {
   description = "The name of the EKS cluster"
   type        = string
